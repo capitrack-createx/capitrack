2353d0e00d56a1e17f28f420c94ce77399503468
Diego <dsuarez34@gatech.edu>
2025-03-10 13:05:13 -0400

    Initial

---
+++ config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBzoOXZwHUtcboDHUwq--rvgA2Zx9UR-tg",
+  authDomain: "createx-8.firebaseapp.com",
+  projectId: "createx-8",
+  storageBucket: "createx-8.firebasestorage.app",
+  messagingSenderId: "404251690411",
+  appId: "1:404251690411:web:9372805f8d2f6a2e0e3eee",
+  measurementId: "G-J58TV8R82M"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
